63b884beae355acd0c689164464a7235a89349f8
SarahBakry <sarah.bakri@elmasartech.com>
2021-11-23 00:02:32 +0200

    add react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.scss";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ItemDetails from "./pages/ItemDetails/ItemDetails";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<ItemDetails />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
